b8c119947449acb53eaecab5d2db45005f796eda
PiotrZak <piotrzak77@gmail.com>
2023-12-13 15:18:25 +0100

    3 day

---
+++ 2023/3/3.py
@@ -0,0 +1,32 @@
+import math as m, re
+
+board = list(open('3.txt'))
+
+
+coords_chars = {(r, c): [] for r in range(140) for c in range(140)
+                    if board[r][c] not in '01234566789.'}
+
+for r, row in enumerate(board):
+    
+    # object with coords -> eg. <re.Match object; span=(63, 66), match='318'>
+    for n in re.finditer(r'\d+', row):
+
+    # this is coords of edge of number:
+    # xxxxx
+    # x343x 
+    # xxxxx
+    # in this case 12 edges (x, y)
+
+        edge = {(r, c) for r in (r-1, r, r+1)
+                       for c in range(n.start()-1, n.end()+1)}
+
+    # (100, 28): [940]
+    # key = (100, 28)
+    # value = 940
+
+    # checking if number is adjacent to symbol
+        for o in edge & coords_chars.keys():
+            coords_chars[o].append(int(n.group()))
+
+print(sum(sum(p)    for p in coords_chars.values()),
+      sum(m.prod(p) for p in coords_chars.values() if len(p)==2))
