3f5ec0782f4406a9c77d0fb1fa73a78a50e87e9f
PiotrZak <piotrzak77@gmail.com>
2023-12-15 18:19:43 +0100

    comments and debug

--- 2023/2/21.py
+++ 2023/2/21.py
@@ -1,7 +1,7 @@
 import re
 
-with open('2.txt', 'r') as f:
-    lines = f.read().split('\n')
+# with open('2.txt', 'r') as f:
+#     lines = f.read().split('\n')
 
 colors = ['red', 'green', 'blue']
 possible_limit = {'red': 12, 'green': 13, 'blue': 14}
@@ -10,6 +10,10 @@ reset_sign = ';'
 game_identifier = ':'
 num_pattern = re.compile(r'(\d+)')
 
+lines = ["Game 1: 1 blue, 2 green, 3 red; 7 red, 8 green; 1 green, 2 red, 1 blue; 2 green, 3 red, 1 blue; 8 green, 1 blue"]
+
+#check if all colors in bag are less than possible limit
+# where bag is dict with colors and quantity (one set - separated by ;)
 def check_if_exceed_limit(bag):
     return all(bag.get(color, 0) <= possible_limit[color] for color in colors)
 
@@ -21,8 +25,13 @@ for game in lines:
     sets_with_game = game.split(reset_sign)
 
     # ['3 blue, 4 red', '1 red, 2 green, 6 blue', '2 green']
-    sets = [s.split(':', 1)[-1].strip() for s in sets_with_game]
 
+    # Split ":" to omit "Game x:"
+    # The strip () method removes any leading, and trailing whitespaces.
+    # The split () method splits a string into a list - to get sets in game in list [1 blue, 2 green, 3 red] 
+
+    sets = [s.split(':', 1)[-1].strip() for s in sets_with_game]
+    
     game_possible = all(
         check_if_exceed_limit({
             color: int(quantity)
